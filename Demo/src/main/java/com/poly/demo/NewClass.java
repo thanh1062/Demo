22709ec1caaec33f4c841d6bab9aaa74145a34ac
thanh1062 <87410424+thanh1062@users.noreply.github.com>
2022-07-22 13:16:57 +0700

    Add files via upload

---
+++ Demo/src/main/java/com/poly/demo/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.poly.demo;
+
+/**
+ *
+ * @author yukth
+ */
+public class NewClass {
+    private int a;
+}
